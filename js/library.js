a3bd5ce071aa990488d45adddc85ebec59568b93
tiger <44923556+eeeeaa@users.noreply.github.com>
2023-10-10 20:35:26 +0700

    Implement add remove and read status
    Implement add and remove book logic and read status toggle

--- js/library.js
+++ js/library.js
@@ -6,7 +6,7 @@ const addBookButton = document.querySelector(".add-book-button");
 const modalDialog = document.querySelector(".modal-box");
 const closeDialogButton = document.querySelector(".close-dialog-button");
 const submitBookButton = document.querySelector(".submit-book-button");
-
+const randomBookButton = document.querySelector(".random-book-button");
 
 function Book(title, author, pageNum, isRead) {
     this.title = title;
@@ -15,8 +15,11 @@ function Book(title, author, pageNum, isRead) {
     this.isRead = isRead;
     this.info = () => {
         let readStatus = (isRead) ? "already read" : "not read yet";
-        return `${title} by ${author}, ${pageNum} pages, ${readStatus}`;
+        return `${title} by ${author}, ${pageNum} pages, ${readStatus}.`;
     };
+    this.changeReadStatus = () => {
+        isRead = !isRead;
+    }
 }
 
 /**
@@ -25,31 +28,102 @@ function Book(title, author, pageNum, isRead) {
  */
 function addBookToLibrary(book) {
     myLibrary.push(book);
+
+    let displayBook =  document.createElement("div");
+    displayBook.setAttribute("data-index", myLibrary.length - 1);
+    displayBook.classList.add("book");
+    displayBook.classList.add("cardAnimation");
+
+    let bookTitle = document.createElement("h2");
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = book.title;
+
+    let bookInfo = document.createElement("div");
+    bookInfo.classList.add("book-info");
+    bookInfo.textContent = book.info();
+
+    let removeButton = createRemoveButton(displayBook);
+
+    let toggle = createReadStatusToggle(displayBook);
+
+    displayBook.appendChild(bookTitle);
+    displayBook.appendChild(bookInfo);
+    displayBook.appendChild(removeButton);
+    displayBook.appendChild(toggle);
+    
+    bookshelf.appendChild(displayBook);
 }
 
-function displayBooks(){
-    for(let i = 0; i < myLibrary.length; i++){
-        let book =  document.createElement("div");
-        book.classList.add("book");
+function createRemoveButton(displayBook){
+    let button = document.createElement("button");
+    button.classList.add("remove-book-button");
+    button.textContent = "Remove this book";
+    button.addEventListener("click", (e)=> {
+        let targetBook = displayBook;
+        let targetIndex = targetBook.getAttribute("data-index");
+        myLibrary.splice(targetIndex, 1);
+        bookshelf.removeChild(targetBook);
 
-        book.textContent = myLibrary[i].info();
+        let books = bookshelf.children;
+        for(let i = 0; i < books.length; i++){
+            let child = books[i];
+            child.setAttribute("data-index", i);
+        }
+        console.log(myLibrary);
+    });
 
-        bookshelf.appendChild(book);
-    }
+    return button;
 }
 
-addBookToLibrary(new Book("Test1", "Me", 16, false));
-addBookToLibrary(new Book("Test2", "Me", 14, true));
-addBookToLibrary(new Book("Test3", "Me", 23, false));
+function createReadStatusToggle(displayBook){
+    let container = document.createElement("div");
+    container.classList.add("toggle-container");
+    
+    let toggleLabel = document.createElement("label");
+    toggleLabel.setAttribute("for","toggle-read-status");
+    toggleLabel.textContent = "Read Status:";
 
-displayBooks();
+    let toggle = document.createElement("input");
+    toggle.classList.add("toggle-read-status");
+    toggle.setAttribute("type","checkbox");
+    toggle.setAttribute("name", "toggle-read-status");
+    
+    let parent = displayBook;
+    let parentIndex = parent.getAttribute("data-index");
+    toggle.checked = myLibrary[parentIndex].isRead;
 
+    toggle.addEventListener("change", (e) => {
+        myLibrary[parentIndex].changeReadStatus();
+        updateBookInfo(displayBook, myLibrary[parentIndex].info());
+    });
+
+    container.appendChild(toggleLabel);
+    container.appendChild(toggle);
+
+    return container;
+}
+
+function updateBookInfo(displayBook, updatedText){
+    displayBook.querySelector(".book-info").textContent = updatedText;
+}
+
+//Listener
 addBookButton.addEventListener("click", (e) => {
     modalDialog.showModal();
 });
 
 closeDialogButton.addEventListener("click", (e) => {
     modalDialog.close();
+});
+
+randomBookButton.addEventListener("click", (e) => {
+    let randBook = new Book(
+        (Math.random() + 1).toString(36).substring(5),
+        (Math.random() + 1).toString(36).substring(5),
+        Math.round((Math.random() + 1) * 10),
+        (Math.random() < 0.5)? true : false
+    )
+    addBookToLibrary(randBook);
 });
 
 submitBookButton.addEventListener("click", (e) => {
@@ -60,5 +134,14 @@ submitBookButton.addEventListener("click", (e) => {
     console.log(`${title.value} ${author.value} ${pageNum.value} ${isRead.value}`);
     if(title.value != "" && author.value != "" && !isNaN(pageNum.value)){
         e.preventDefault();
+        addBookToLibrary(
+            new Book(
+                title.value,
+                author.value,
+                pageNum.value,
+                isRead.checked
+            )
+        );
+        modalDialog.close();
     }
 });
